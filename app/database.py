584fdc6032c7c6c61b73ff200e9412d7b4de915b
Jorge2018 <Jorge2018@users.noreply.github.com>
2026-02-26 18:35:32 -0300

    Subida inicial

--- app/database.py
+++ app/database.py
@@ -6,10 +6,14 @@ from sqlalchemy.orm import sessionmaker
 # Si corres la app en Windows, usa localhost. 
 # Si la corres dentro de Docker, el host será 'db'.
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://usuario_devops:password_seguro@localhost:5432/notas_db"
+    "DATABASE_URL", "sqlite:///./test.db" # Si no hay URL, usa SQLite
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Si es SQLite, necesita un argumento extra
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
